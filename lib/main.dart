4eadb09415091370fe3e5b6f9326ded7ee58824e
TheRealHex <devilxnp@gmail.com>
2024-02-18 14:22:02 +0545

    ui: change

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: const Home(),
+      theme: ThemeData.dark(),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,3 @@
-import 'package:bmi_calculator/screens/result.dart';
-import 'package:bmi_calculator/widgets/input.dart';
-import 'package:bmi_calculator/widgets/label.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -8,6 +5,8 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController heightController = TextEditingController();
+    TextEditingController weightController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
@@ -21,47 +20,35 @@ class Home extends StatelessWidget {
         ),
       ),
       body: Container(
-        width: double.infinity,
-        height: double.infinity,
         decoration: const BoxDecoration(color: Colors.black),
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Row(
-              children: [
-                Spacer(),
-                Label(text: 'Height'),
-                TextInputField(hint: 'ft'),
-                TextInputField(hint: 'inch'),
-                Spacer(),
-              ],
-            ),
-            const Row(
-              children: [
-                Spacer(),
-                Label(text: 'Weight'),
-                TextInputField(hint: 'kg'),
-                Spacer(),
-              ],
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 50),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.blue,
-                  textStyle: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => const Result()),
-                  );
-                },
-                child: const Text('Calculate'),
-              ),
-            ),
+            textField(heightController, 'Height (ft)', '6.1'),
+            textField(weightController, 'Weight (kg)', '70'),
+            const SizedBox(height: 10),
+            ElevatedButton(onPressed: () {}, child: const Text('Calculate'))
           ],
+        ),
+      ),
+    );
+  }
+
+  textField(TextEditingController controller, String label, String hint) {
+    return SizedBox(
+      width: 200,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: TextField(
+          controller: controller,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            label: Text(label),
+            hintText: hint,
+            alignLabelWithHint: true,
+          ),
         ),
       ),
     );
